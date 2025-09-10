3f2d3e40c0c16db801ca775e8ea0eba8e18b9c93
cvClaudiu <xenon@LAPTOP-LQVONC54>
2025-09-10 14:33:29 +0300

    initial commit, added the project

---
+++ WebDice/Controllers/RanController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebRand.Services;
+
+namespace WebRand.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RanController : ControllerBase
+    {
+        private readonly RollService _rollService;
+        public RanController(RollService rollService)
+        {
+            _rollService = rollService;
+        }
+        [HttpGet]
+        public IActionResult Get([FromQuery] int? min, [FromQuery] int? max)
+        {
+            int actualMin = min ?? 0;                
+            int actualMax = max ?? int.MaxValue;
+            var rolled = _rollService.Roll(actualMin, actualMax);
+
+            return Ok(new { min = actualMin, max = actualMax, rolled });
+        }
+
+    }
+}
---
+++ WebDice/Services/RollService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace WebRand.Services
+{
+    public class RollService
+    {
+        private readonly Random _rn = new();
+        
+        public int Roll(int RNmin, int RNmax)
+        {
+            if (RNmin > RNmax)
+                throw new ArgumentException("The minimum value cannot be greater than the maximum value.");
+            if (RNmin == RNmax)
+                return RNmin;
+            if (RNmax == int.MaxValue)
+                return RNmin + _rn.Next(int.MaxValue - RNmin);
+
+            return _rn.Next(RNmin, RNmax + 1);
+        }
+    }
+}
